7e0e1c833a6ad316a51e0298d2f032600877230a
Kai Mayer <mail@kazuto.de>
2023-08-24 15:00:45 +0200

    Add snippets and fix color issues (#2)
    * feat: update colors
    
    * feat: logo and footer
    
    * fix: color issues

--- components/logo.vue
+++ components/logo.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="mb-8 block text-secondary-400 dark:text-secondary-500">
-    <a href="/">
-      <nuxt-img
-        src="/logo.svg"
-        class="h-auto w-12"
-      />
+    <a
+      href="/"
+      class="block h-auto w-12"
+    >
+      <nuxt-img src="/logo.svg" />
     </a>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
+<style scoped></style>
 <style scoped></style>
